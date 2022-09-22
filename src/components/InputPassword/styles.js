b4177676091f3623a7f49eb1ecf3440b455686b7
madrigrano78 <gabrielmmrodrigues@hotmail.com>
2022-09-22 17:50:33 -0300

    Implementacao dos inputs da tela de login

---
+++ src/components/InputPassword/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  justify-content: left;
+`;
+
+export const InputField = styled.input`
+  height: 44px;
+  width: 340px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  background-color: white;
+  border: 1px solid grey;
+  padding-left: 10px;
+  line-height: normal;
+  opacity: 0.4;
+`;
